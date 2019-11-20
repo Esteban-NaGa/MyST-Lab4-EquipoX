4cca929ece2faea9f0d90b9dfa130e3cbd2976ed
Esteban Navarrete <esteban.ng97@gmail.com>
2019-11-20 00:57:19 -0600

    Avance y correciones

--- Lab4_Intro.py
+++ Lab4_Intro.py
@@ -7,13 +7,13 @@ Created on Thu Nov  7 08:16:17 2019
 """
 
 import pandas as pd
-import numpy as np
-import datetime as datetime
+#import numpy as np
+#import datetime as datetime
 #%%
-#precios = '/Users/Esteban/Desktop/MyST_Python/precios_historicos_eurusd.csv'
-#calendario = '/Users/Esteban/Desktop/MyST_Python/calendario_economico.csv'
-precios = '/Users/preci/Documents/iteso/semestre 9/labs trading/Lab4/MyST-Lab5-EquipoX/precios_historicos_eurusd.csv'
-calendario = '/Users/preci/Documents/iteso/semestre 9/labs trading/Lab4/MyST-Lab5-EquipoX/calendario_economico.csv'
+precios = '/Users/Esteban/Desktop/MyST_Python/precios_historicos_eurusd.csv'
+calendario = '/Users/Esteban/Desktop/MyST_Python/calendario_economico.csv'
+#precios = '/Users/preci/Documents/iteso/semestre 9/labs trading/Lab4/MyST-Lab5-EquipoX/precios_historicos_eurusd.csv'
+#calendario = '/Users/preci/Documents/iteso/semestre 9/labs trading/Lab4/MyST-Lab5-EquipoX/calendario_economico.csv'
 
 #%%
 precios_df = pd.read_csv(precios,
@@ -57,17 +57,18 @@ for i in range(len(ce_df)):
 Autos = Autos.T
 Control = Control.T
 df_indicadores = pd.concat([Autos, Control])
+Autos = Autos.reset_index(drop=True)
+Control = Control.reset_index(drop=True) 
+#%%
+Autos['Volatility']=Autos['Volatility'].astype(object).astype(int)
+Autos.iloc[:,5:9]=Autos.iloc[:,5:9].astype(object).astype(float)
 
+Control['Volatility']=Control['Volatility'].astype(object).astype(int)
+Control.iloc[:,5:9]=Control.iloc[:,5:9].astype(object).astype(float)
 #%%
-#Usar np.where para encontrar el match entre precios y calendario
-#%%
-#A 	actual >= previous & actual >= consensus & consensus >= previous
-#B 	actual >= previous & actual >= consensus & consensus < Precious
-#C 	actual >= previous & actual < consensus & consensus >= previous
-#D 	actual >= previous & actual < consensus & consensus < previous
 def escenario(d):
-    d["actual"]=d["escenario"]
-    for i in range(len(d["actual"])): 
+    d["escenario"]=d["actual"]
+    for i in range(len(d)): 
       if d["actual"][i] >= d["consensus"][i] >= d["previous"][i]:
           d["escenario"][i] = "A"
       elif d["actual"][i] >= d["consensus"][i] < d["previous"][i]:
@@ -79,18 +80,35 @@ def escenario(d):
           
     return d
 #%%
-Autos = escenario(Autos)
-Control = escenario(Control)
-df_indicadores = pd.concat([Autos, Control])
+Autos_esce = escenario(Autos)
+Control_esce = escenario(Control)
+df_indicadores_esce = pd.concat([Autos, Control])
 #%% match de nuestros indicadores con el precio
 comparacion_autos = precios_df[precios_df.timestamp.isin(Autos.timestamp)]
 comparacion_control = precios_df[precios_df.timestamp.isin(Control.timestamp)]
-
 #%%
 ## comprehension de listas para data frames
 autoscom = [precios_df.iloc[i-6:i+7] for i in comparacion_autos.index]
 controlcom = [precios_df.iloc[i-6:i+7] for i in comparacion_autos.index]
+#%%
+Autos_esce['direccion'] = Autos['actual']
+Autos_esce['pips alcistas'] = Autos['actual']
+Autos_esce['pips bajistas'] = Autos['actual']
+Autos_esce['volatilidad'] = Autos['actual']
 
-
-
-
+Control_esce['direccion'] = Control['actual']
+Control_esce['pips alcistas'] = Control['actual']
+Control_esce['pips bajistas'] = Control['actual']
+Control_esce['volatilidad'] = Control['actual']
+#%%
+def df_final(df, periodos):
+    
+     for i in range(len(df)):
+        df['direccion'][i] = periodos[i]["close"].iloc[-1] - periodos[i]["open"].iloc[6]
+        df['pips alcistas'][i] = periodos[i]["high"].iloc[6:-1].max() - periodos[i]["open"].iloc[0]
+        df['pips bajistas'][i] = periodos[i]["open"].iloc[0] - periodos[i]["low"].iloc[6:-1].min()
+        df['volatilidad'][i] = periodos[i]["high"].max()-periodos[i]["low"].min()
+        return df
+#%%
+Final_autos = df_final(Autos_esce, autoscom)
+Final_control = df_final(Control_esce, controlcom)
